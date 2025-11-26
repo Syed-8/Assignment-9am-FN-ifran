41b32b97c1dae2556695c93bc2b11757aa20c652
Syed-8 <irfansyed44730@gmail.com>
2025-11-27 00:20:25 +0530

    Task 17

---
+++ Day 17 JS/script1.js
@@ -0,0 +1,27 @@
+
+console.log("i'm irfan");
+
+let age = 22;
+console.log("my age is:",22);
+
+function SayHello(){
+    console.log("Hello Good Morning");
+}
+SayHello();
+
+
+        function Hello(){
+        document.getElementById("Hy").innerText="Text changed by JavaScript";
+        }
+
+console.log("My Favorite hobby is:Photography" );
+console.log("My Favorite food is :Briyani");
+console.log("My Favorite color is: white");
+    
+console.log("---About me---");
+console.log("i'm irfan");
+console.log("my age is:",22);
+console.log("My Favorite hobby is:Photography" );
+console.log("My Favorite food is :Briyani");
+console.log("My Favorite color is: white");
+
