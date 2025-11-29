a1712c8ce749c54e5a0c1612aa645319264eb6a2
Syed-8 <irfansyed44730@gmail.com>
2025-11-29 21:26:54 +0530

    Task 18 js

---
+++ Day 18 JS/script.js
@@ -0,0 +1,130 @@
+//TAsk 1
+/*
+
+var name = "java";
+console.log(name);
+
+ 
+let name = "irfan";
+name="syed";
+console.log(name);
+
+
+
+const name = "photography";
+console.log(name);
+
+
+
+// Task 2
+
+let a= 10;
+let a= 20;
+console.log(a);
+
+
+//Task 3
+
+const age = "30";
+
+
+
+ //Task 4
+
+let name = "abdullah";
+
+{
+    let name = "irfan";
+console.log(name);
+
+}
+console.log(name);
+
+
+
+//Task 5
+
+const bag = {
+    books:"atomic habits",copy:25
+}
+
+bag.books="java";
+console.log(bag);
+
+
+
+//Task 6
+
+const updateprofile = {
+    name:"syed",
+    age: 25,
+    email:"irfansyed",
+    location:"chennai",
+};
+console.log(updateprofile);
+
+updateprofile.age=22;
+updateprofile.location = "Ashoknagar";
+console.log(updateprofile);
+
+
+
+//Task 7
+ let num1 = 40;
+ let num2= 10;
+ let results;
+
+results= num1 + num2;
+console.log("Addition",results);
+results= num1 - num2;
+console.log("subraction",results);
+results= num1 * num2;
+console.log("multiplication",results);
+results= num1 / num2;
+console.log("division",results);
+
+
+
+//Task 9
+
+const cartItems={
+   name :"Apple",
+   product:"Airpods",
+   price : 2500,
+   quantity : 2,
+};
+console.log( "befor update:",cartItems);
+
+cartItems.quantity = 5;
+console.log("after update:",cartItems);
+
+*/
+// Task 10
+
+const defaultSettings={
+    theme:"dark",
+    fontsize:14,
+    Notification: true,
+}
+
+console.log("defaultSettings:",defaultSettings);
+
+let userSettings = {
+    theme:"dark",
+    fontsize:14,
+    Notification:true,
+}
+userSettings.theme = "light",
+userSettings.fontsize = 22,
+userSettings.Notification = false
+
+console.log("userSettings after change:",userSettings);
+
+
+
+
+
+
+
+
+
