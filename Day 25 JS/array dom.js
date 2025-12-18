a7cbc6c8312b66ab2fc7e0b453854bb1eda9b0b1
Syed-8 <irfansyed44730@gmail.com>
2025-12-18 23:33:29 +0530

    Task25

---
+++ Day 25 JS/array dom.js
@@ -0,0 +1,72 @@
+// Task 1
+
+// let colors = ["REd","Blue","Yellow","White","Green"]
+
+// console.log("Frist colors:",colors[0]);
+// console.log("Last color:",colors[4]);
+
+//Task 2
+
+// let items = ["Book","Pencil","pen"]
+// items.push("Eraser");
+// console.log("Push:",items);
+
+// items.pop();
+// console.log("Pop:",items);
+
+// items.shift();
+// console.log("Shift:",items);
+
+// items.unshift("Sketch");
+// console.log("unshift:",items);
+
+//Task 3
+
+// let marks = [40,59,89,73,60];
+// marks.forEach(m=>console.log(m));
+
+//Task 4
+
+// let marks = [4,5,9,3,6];
+// marks.map(m=>console.log(m*2));
+
+//Task 5
+
+// let passed = [60,89,95,35];
+// passed.filter (m=> console.log (m>=45));
+
+//Task 6
+
+// let numbers = [30,40,60,50,20];
+
+// let total = numbers.reduce (function(accumulator,currentValue){
+//     return accumulator+currentValue;} ,0);
+
+//     console.log("Total:",total);
+
+//Task 7
+// let ages = [9,12,15,17,18,26,32,46,57];
+//  let adults = ages.filter(function(age){
+//     return age >=18;
+//  });
+//  console.log("Adults:",adults);
+ 
+//Task 8
+
+// let students = [
+//     ["Ajay",65],
+//     ["zaya",59],
+//     ["Imam",78],
+//     ["yash",81],
+//     ["shahin",50]
+// ];
+// students.forEach(function(students){
+//     console.log("Name:", students[0], "Marks:", students[1]); 
+// });
+
+//Task 9
+// let arr1 = [10,20,30];
+// let arr2 = [40,50,60];
+
+// let combined1 = [...arr1,...arr2];
+// console.log("combined:",combined1);
