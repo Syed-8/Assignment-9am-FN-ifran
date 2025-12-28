7ca3a77f139cffc6e2806267a228c57bf0264033
Syed-8 <irfansyed44730@gmail.com>
2025-12-28 20:06:48 +0530

    Task 28

---
+++ Day 28 JS/errors.js
@@ -0,0 +1,37 @@
+// Task 1
+
+if (x > 10 {
+console.log("Big number");
+}
+
+// Task 2
+
+let name = "";
+console.log(name.toUpperCase());
+
+// Task 3
+
+ if (age >= 18) {
+  console.log("Eligible to vote");
+} else {
+  console.log("Not eligible");
+}
+
+//Task 4
+
+let =90
+console.log(total);
+
+//Task 5
+
+function add(a, b) {
+  return a + c;
+}
+console.log(add(5, 3));
+
+// Task 9
+
+function greet() {
+  console.log("Hello " + name);
+}
+greet();
