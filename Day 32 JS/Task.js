2d6f676ef01002a983d9530074937f7aba392545
Syed-8 <irfansyed44730@gmail.com>
2026-01-28 00:10:42 +0530

    Task 32

---
+++ Day 32 JS/Task.js
@@ -0,0 +1,44 @@
+// 1. A React conponent that displays tree reprensents the hierachical structure of companies in an applcation
+// is shows how a parents components passess data due to child components using props.
+// Example sstructure 0f companies
+// App (root commponents )Header,Main,,Sidebar,Content,Card,Footer
+
+//2 . * look for repeat UI elements (buttons,cards,lists).
+// *separate sections like header ,footer,navigation,main content.
+// *identify parts that manage thier own data or logic .
+// * Group related elements into a single component.
+
+//3. A Static HTML page converted into React components by breaking it application easier to maintain and scale
+// Identify page sections (Header, Navbar, Content, Footer).
+// Create a separate component for each section.
+// Replace static data with props.
+// Manage dynamic behavior using state and events.
+
+//4. Page Reload -No full page reload-Full page reload
+// Performance -Faster after first load -Slower due to reloads
+// User Experience -Smooth and interactive -Less interactive
+// Routing -Client-side routing -Server-side routing
+// Technology -React, Angular, Vue - HTML, CSS, server templates
+
+//5. The React ecosystem includes tools and libraries that support development:
+// Create React App / Vite – Project setup
+// React Router – Navigation and routing
+// Redux / Context API – State management
+// Axios / Fetch API – API communication
+// Babel – JavaScript compiler
+// Webpack – Module bundler
+// ESLint – Code quality checking
+// Jest / React Testing Library – Testing
+
+// 6.React is a JavaScript library used to build fast, interactive, and reusable  user interfaces, mainly for single - 
+// page applications.It allows developers to create UI using component and efficiently update the view when data changes.
+
+//7. Component-Based Architecture – UI is divided into reusable components, making code easy to manage.
+// Fast Performance – Uses Virtual DOM to minimize direct updates to the real DOM.
+// One-Way Data Binding – Makes data flow predictable and easier to debug
+
+//8.A Single Page Application (SPA) is a web application that loads only one HTML page and dynamically updates the content without reloading the entire page.
+// SPAs provide a smooth user experience and are commonly built using frameworks like React.
+
+//10.The Virtual DOM is a lightweight copy of the real DOM maintained by React.
+// When changes occur, React first updates the Virtual DOM, compares it with the previous version, and then updates only the changed parts in the real DOM, improving performance.
